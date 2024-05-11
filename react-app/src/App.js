1dd2145b577e48e2ab9a2670e20f2c9c2d77fffc
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-11 16:54:56 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
 import React, {useState } from 'react'
+import "./App.css"
 
 export default function App() {
   const [count, setcount] = useState(0);
   return (
-    <div>
+    <div className='App'>
 
       <h1>Count is {count}</h1>
       <button onClick={()=>setcount(count+1)}>+</button>

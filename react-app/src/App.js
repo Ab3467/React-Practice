d9aa2f2dd900e0cd866c27df9613040f1bd5f307
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-11 17:14:41 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,16 @@ import "./App.css"
 
 export default function App() {
   const [count, setcount] = useState(0);
+
+  function handleReset(){
+    setcount(0)
+  }
   return (
     <div className='App'>
 
       <h1>Count is {count}</h1>
       <button onClick={()=>setcount(count+1)}>+</button>
+      <button onClick={handleReset}>Reset</button>
       
     </div>
   )

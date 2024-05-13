e193652d9bb987cf5432f63fb02c1597c0c9d2ed
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-13 18:10:41 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,28 @@ import "./App.css"
 export default function App() {
   const [count, setcount] = useState(0);
 
+  const [todo,settodo] = useState([
+    {
+      title : 'Todo 1',
+      description : 'hey I am 1st',
+    },
+    {
+      title : 'Todo 2',
+      description : 'hey I am 2nd',
+    },
+    {
+      title : 'Todo 3',
+      description : 'hey I am 3rd',
+    }
+  ])
+
+
+  const Todo = ({todo}) =>{
+    return
+    <>
+    
+    </>
+  }
 
   function handleReset(){
     setcount(0)
@@ -21,7 +43,8 @@ export default function App() {
 
       <h1>Count is {count}</h1>
       <button onClick={()=>setcount(count+1)}>+</button>
-      {count ?<button onClick={handleReset}>Reset</button>: "Nothing"}
+      {count && <button onClick={handleReset}>Reset</button>}             {/*conditional rendering */}
+       {/* {count ?<button onClick={()=> setcount(!count)}>Reset</button>: "Nothing"}    */}
       
     </div>
   )

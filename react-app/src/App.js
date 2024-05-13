5100b1e1ac51919915e155aa445ef83c133d989d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-13 19:08:59 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,55 +3,34 @@ import "./App.css"
 
 
 export default function App() {
-  const [count, setcount] = useState(0);
+  const [count, setcount] = useState([]);
 
-  const [todos,settodos] = useState([
-    {
-      title : 'Todo 1',
-      description : 'hey I am 1st',
-    },
-    {
-      title : 'Todo 2',
-      description : 'hey I am 2nd',
-    },
-    {
-      title : 'Todo 3',
-      description : 'hey I am 3rd',
-    }
-  ])
+ 
+ let fetchData = ()=>{
+  let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+  let data = a.json();
+ }
+  // function handleReset(){
+  //   setcount(0)
+  // }
 
-
-  const Todo = ({todo}) =>{
-    return  (
-    <div className="m-4 text-slate-600 border border-1 border-black">
-     <div className="todo">{todo.title}</div>
-     <div className="todo">{todo.description}</div>
-     </div>
-    
-    )
-  }
-
-  function handleReset(){
-    setcount(0)
-  }
-
-  let a = useRef(0);    // useRef() rerendering p a ki value ko again 0 ni kry ga 
-  useEffect(() => {
-      a.current= a.current+1;
-      console.log(`Value of a is ${a.current}`);
-  },[count]);
+  // let a = useRef(0);    // useRef() rerendering p a ki value ko again 0 ni kry ga 
+  // useEffect(() => {
+  //     a.current= a.current+1;
+  //     console.log(`Value of a is ${a.current}`);
+  // },[count]);
   
   return (
-    <div className='App'>
+    // <div className='App'>
 
-      <h1>Count is {count}</h1>
-      <button onClick={()=>setcount(count+1)}>+</button>
-      {count && <button onClick={handleReset}>Reset</button>}             {/*conditional rendering */}
-       {/* {count ?<button onClick={()=> setcount(!count)}>Reset</button>: "Nothing"}    */}
+    //   <h1>Count is {count}</h1>
+    //   <button onClick={()=>setcount(count+1)}>+</button>
+    //   {count && <button onClick={handleReset}>Reset</button>}             {/*conditional rendering */}
+    //    {/* {count ?<button onClick={()=> setcount(!count)}>Reset</button>: "Nothing"}    */}
 
-       {todos.map(todo=>{
-        return <Todo todo={todo} key={todo.title}/>
-        })}
+    //    {todos.map(todo=>{
+    //     return <Todo todo={todo} key={todo.title}/>
+    //     })}
       
     </div>
   )

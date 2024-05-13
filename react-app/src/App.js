6e465e7a955285a4dce0ee33cde048b4d7e879b3
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-13 19:21:09 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,35 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, {useState,useEffect,useRef } from 'react'
+import "./App.css"
+
 
 export default function App() {
-  const [cards, setCards] = useState([]);
+  const [cards, setcards] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json();
-      setCards(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+ 
+ let fetchData = async()=>{
+  let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+  let data = await a.json();
+  setcards(data);
+ }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+ useEffect(() => {
+  fetchData();
+ }, [])
+ 
+ 
+  
   return (
     <div className='container'>
-      {cards.map((card) => (
-        <div className='card' key={card.id}>
-          <h1>{card.id}</h1>
-          <p>{card.body}</p>
-          <span>By: User ID {card.userId}</span>
-        </div>
-      ))}
+    {cards.map((card)=>{
+       return <div className="card" key={card.id}>
+       <h1>{card.id}</h1>
+       <p>{card.body}</p>
+       <span>By: UserId {card.userId}</span>
+       </div>
+      })}
     </div>
-  );
+ 
+  )
 }
+
+
+
+// I will work on it but on Monday
+

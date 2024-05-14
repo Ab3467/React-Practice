c2634aeecb7782aeecfe58ddcd3ee69dca2d5e8c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 21:53:51 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -34,9 +34,12 @@ export default function Navbar() {
         <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
          ))}
         </ul>
-        <div onClick={()=> setNav(!nav)} className=" cursor-pointer">
+        <div onClick={()=> setNav(!nav)} className=" cursor-pointer pr-4 z-10 text-gray-500">
            {nav ? <FaBars size={30}/> : <FaTimes size={30}/>}
         </div>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-grey-800 text-grey-500'>
+
+        </ul>
     </div>
   )
 }

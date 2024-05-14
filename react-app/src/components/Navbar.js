e2879b183f555b8c6e4488151374db81e6950a07
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 19:57:02 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -3,6 +3,8 @@ import {FaBars,FaTimes} from 'react-icons/fa'
 
 export default function Navbar() {
   return (
-    <div className=' flex justify-between items-center w-full h-20 px-4 bg-black'></div>
+    <div className=' flex justify-between items-center w-full h-28 px-4 text-white bg-black fixed'>
+        <h1 className='text-5xl font-signature ml-2'>Portfolio</h1>
+    </div>
   )
 }

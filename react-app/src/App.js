b0542919fd3cb66d77d3de97f9305230725c0e7d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 19:54:10 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar'
 export default function App() {
   return (
     <div>
-      <h1 className=' text-5xl font-bold font-signature m-4'>Portfolio</h1>
       <Navbar/>
     </div>
   )
---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FaBars,FaTimes} from 'react-icons/fa'
+
+export default function Navbar() {
+  return (
+    <div className=' flex justify-between items-center w-full h-20 px-4 text-white bg-black'></div>
+  )
+}

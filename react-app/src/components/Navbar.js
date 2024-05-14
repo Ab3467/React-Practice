c297bf37977b540f35c8845a078a67938caf063e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 20:17:22 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -28,12 +28,14 @@ export default function Navbar() {
     <div className=' flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
        <div><h1 className='text-5xl font-signature ml-2'>Portfolio</h1></div> 
 
-       <ul className='flex'>
+       <ul className=' hidden md:flex'>
         {links.map(({id,link})=>(
         <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
          ))}
         </ul>
-
+        <div className=" cursor-pointer">
+            <FaBars size={30}/>
+        </div>
     </div>
   )
 }

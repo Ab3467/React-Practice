386b41f128c73de4e2fbe45e15781dfeec972047
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 19:09:24 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className=' text-teal-400 font-bold font-signature'>Hello!</h1>
+      <h1 className=' text-5xl font-bold font-signature m-4'>Hello!</h1>
     </div>
   )
 }

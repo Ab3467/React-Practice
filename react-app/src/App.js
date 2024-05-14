376ea5d1b8789a924461243679f2fcf14399da42
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 19:46:39 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div>
       <h1 className=' text-5xl font-bold font-signature m-4'>Portfolio</h1>
+      <Navbar/>
     </div>
   )
 }

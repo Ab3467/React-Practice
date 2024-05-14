d7b87b17db98985fe61ee4117c19b0610908b3fe
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 18:38:17 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,36 +1,13 @@
 import React from 'react'
 
-
-
 export default function App() {
-  const [cards, setcards] = useState([]);
-
- 
- let fetchData = async()=>{
-  let a = await fetch("https://jsonplaceholder.typicode.com/posts");
-  let data = await a.json();
-  setcards(data);
- }
-
- useEffect(() => {
-  fetchData();
- }, [])
- 
- 
-  
   return (
-    <div className='container'>
-    {cards.map((card)=>{
-       return <div className="card" key={card.id}>
-       <h1>{card.id}</h1>
-       <p>{card.body}</p>
-       <span>By: UserId {card.userId}</span>
-       </div>
-      })}
+    <div>
+      
     </div>
- 
   )
 }
+
 
 
 

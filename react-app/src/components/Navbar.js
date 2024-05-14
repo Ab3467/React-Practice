0e861d8e41c03c2a6c6a897fbd70793a9f0e787c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-14 20:11:20 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -25,12 +25,15 @@ export default function Navbar() {
         },
     ]
   return (
-    <div className=' flex justify-between items-center w-full h-28 px-4 text-white bg-black fixed'>
+    <div className=' flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
        <div><h1 className='text-5xl font-signature ml-2'>Portfolio</h1></div> 
+
        <ul className='flex'>
-        <li className='px-4 cursor-auto capitalize font-medium text-gray-500 hover:scale-105 duration-200'>Home</li>
-        <li>About</li>
-       </ul>
+        {links.map(({id,link})=>(
+        <li key={id} className='px-4 cursor-auto capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
+         ))}
+        </ul>
+
     </div>
   )
 }

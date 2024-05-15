e5067439d4c729edef185d22fe53ec2471a65e5e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 18:18:34 +0500

    Changes

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Image from "../assets/Ahmad.png"
+import {MdOutlinekeyboardArrowRight} from "react-icons/md"
 
 export default function Home() {
   return (
-    <div>
-      HOME
+    <div name="home" className='h-screen w-full bg-gradient-to-b from to-gray-800'>
+      <div className="">
+        <div className="">
+            <h1>Hi, I'm Frontend Web developer</h1>
+        </div>
+      </div>
     </div>
   )
 }

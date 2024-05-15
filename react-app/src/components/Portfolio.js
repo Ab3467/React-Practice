c9d7408e67e01170b0b5d2debcaf703169a3a82f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 21:38:23 +0500

    Changes

--- react-app/src/components/Portfolio.js
+++ react-app/src/components/Portfolio.js
@@ -7,6 +7,32 @@ import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
 import reactWeather from "../assets/portfolio/reactWeather.jpg"
 
 export default function Portfolio() {
+    const portfolios = [
+        {
+            id:1,
+            src: arrayDestruct
+        },
+        {
+            id:2,
+            src: installNode
+        },
+        {
+            id:3,
+            src: navbar
+        },
+        {
+            id:4,
+            src: reactParallax
+        },
+        {
+            id:5,
+            src: reactSmooth
+        },
+        {
+            id:6,
+            src: reactWeather
+        },
+    ]
     return (
         <div className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen' name="portfolio">
             <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -16,7 +42,7 @@ export default function Portfolio() {
                 </div>
                 <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
                     <div className=' shadow-sm shadow-gray-600 rounded-lg'>
-                        <img src={reactWeather} alt="port" className=' rounded-md duration-200 hover:scale-105'/>
+                        <img src={src} alt="port" className=' rounded-md duration-200 hover:scale-105'/>
                     <div className=' flex items-center justify-center'>
                         <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
                         <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>

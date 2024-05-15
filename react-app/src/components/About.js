daa46a48462167cd71adaf2737bec3f499d39206
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 20:44:55 +0500

    Changes

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            <div>
-                <div>
-                    <p>About</p>
+        <div className=' w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white' name="About">
+            <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
-                <p>
+                <p className='text-xl mt-20'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero amet sit aspernatur laboriosam consequatur. Expedita, temporibus culpa molestiae eligendi fuga dignissimos, hic earum dolorem non consectetur modi accusantium numquam maiores odio minima reprehenderit!
                 </p>
                 <br/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quam adipisci omnis ratione quia similique asperiores blanditiis, doloribus eos? Quod quas iusto ab aspernatur ducimus hic, expedita tempora minima architecto facilis sint eaque placeat aperiam cum harum nisi ullam dolores magnam laboriosam. Placeat tempora veritatis minus suscipit omnis cumque quas.</p>
+                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quam adipisci omnis ratione quia similique asperiores blanditiis, doloribus eos? Quod quas iusto ab aspernatur ducimus hic, expedita tempora minima architecto facilis sint eaque placeat aperiam cum harum nisi ullam dolores magnam laboriosam. Placeat tempora veritatis minus suscipit omnis cumque quas.</p>
             </div>
         </div>
     )

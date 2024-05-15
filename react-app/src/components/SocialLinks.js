9e87bccba3e7ab45c8feaac3ee93ad3178b53aa0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:52:24 +0500

    Changes

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -31,7 +31,7 @@ export default function SocialLinks() {
                Mail <HiOutlineMail size={30}/>
                </>
             ),
-            href: "https://www.linkedin.com/in/ahmad-abu-bakr-481270254/"
+            href: "ahmed.nums345@gmail.com"
           },
           {
             id:4,
@@ -40,7 +40,7 @@ export default function SocialLinks() {
               Resume <BsFillPersonLinesFill size={30}/>
              </>
             ),
-            href: "https://www.linkedin.com/in/ahmad-abu-bakr-481270254/",
+            href: "",
             style: 'rounded-br-md',
             download: true
           },
@@ -50,9 +50,9 @@ export default function SocialLinks() {
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
        {links.map((id,child,download,href,style)=>(
-<li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] rounded-md bg-gray-500 hover:rounded-none duration-500'+ " " +style}>
+    <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] rounded-md bg-gray-500 hover:rounded-none duration-500'+ " " +style}>
         <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
-        <> {link} <FaLinkedin size={30}/></>
+        <> {child} <FaLinkedin size={30}/></>
         </a>
     </li>
        ))}

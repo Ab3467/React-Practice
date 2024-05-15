05129d3edf0d21a139ce1a091422f0861a8c91a7
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 18:34:07 +0500

    Changes

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import Image from "../assets/Ahmad.png"
-import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+import React from 'react';
+import Image from "../assets/Ahmad.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
-      <div className=" max-w-screen-lg mx-auto flex justify-center items-center flex-col h-full px-4 md:flex-row">
-        <div className="">
-            <h1 className=' text-slate-50'>Hi, I'm Frontend Web developer</h1>
-            <p>I have 1 year of experience in frontend web development.
-              
-            </p>
-            <div className="">
-                <button>portfolio
-                    <span><MdOutlineKeyboardArrowRight/></span>
-                </button>
-            </div>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white'>
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+        <div className="flex flex-col justify-center h-full md:w-1/2">
+          <h1 className='text-4xl sm:text-7xl font-bold text-white'>Hi, I'm a Frontend Web Developer</h1>
+          <p className='py-4 max-w-md'>
+            I have 1 year of experience in frontend web development.
+            Currently working on reactjs and tailwind. Also doing an 
+            Internship at Vyro
+          </p>
+          <div>
+            <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+              Portfolio
+              <span className='ml-1'>
+                <MdOutlineKeyboardArrowRight size={25} />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="md:w-1/2">
+          <img src={Image} alt="my_img" className='rounded-2xl mx-auto w-2/3 md:w-full' />
         </div>
       </div>
     </div>
-  )
+  );
 }

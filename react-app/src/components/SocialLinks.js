3c0ef88c9836e6d90e2078ae96d72add8ff83593
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:19:38 +0500

    Changes

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -7,16 +7,16 @@ export default function SocialLinks() {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
+        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px]'>
             <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
             <> Linkedin <FaLinkedin size={30}/></>
             </a>
         </li>
-        <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
+        {/* <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500'>
             <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
-            <> Github <FaGithub size={30} className='pt-3'/></>
+            <> Github <FaGithub size={40} className='pt-2 pl-3'/></>
             </a>
-        </li>
+        </li> */}
       </ul>
     </div>
   )

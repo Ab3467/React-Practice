586a466a733b55645dc8b2351cc53affbf13c1a5
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:28:52 +0500

    Changes

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -7,7 +7,7 @@ export default function SocialLinks() {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] rounded-md bg-gray-500 hover:rounded-none duration-300 border hover:border-cyan-100'>
+        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] rounded-md bg-gray-500 hover:rounded-none duration-300 hover:border hover:border-black'>
             <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
             <> Linkedin <FaLinkedin size={30}/></>
             </a>

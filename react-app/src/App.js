9c037fa8f4c5d118ba7e85049ed32256a9be14a1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 21:15:07 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
         <Home />
         <About />
         <SocialLinks />
-
+        <Portfolio/>
       </div>
 
     </>
--- react-app/src/components/Portfolio.js
+++ react-app/src/components/Portfolio.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg"
+import installNode from "../assets/portfolio/installNode.jpg"
+import navbar from "../assets/portfolio/navbar.jpg"
+import reactParallax from "../assets/portfolio/reactParallax.jpg"
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
+import reactWeather from "../assets/portfolio/reactWeather.jpg"
 
 export default function Portfolio() {
   return (

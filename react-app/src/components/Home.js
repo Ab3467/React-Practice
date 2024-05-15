eb566c4fb24c9246e8fdeb8de38101e4bd25db2f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 18:27:28 +0500

    Changes

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Image from "../assets/Ahmad.png"
-import {MdOutlinekeyboardArrowRight} from "react-icons/md"
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
 
 export default function Home() {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from to-gray-800'>
-      <div className="">
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+      <div className=" max-w-screen-lg mx-auto flex justify-center items-center flex-col h-full px-4 md:flex-row">
         <div className="">
-            <h1>Hi, I'm Frontend Web developer</h1>
+            <h1 className=' text-slate-50'>Hi, I'm Frontend Web developer</h1>
+            <p>I have 1 year of experience in frontend web development.
+              
+            </p>
+            <div className="">
+                <button>portfolio
+                    <span><MdOutlineKeyboardArrowRight/></span>
+                </button>
+            </div>
         </div>
       </div>
     </div>

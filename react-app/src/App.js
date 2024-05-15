54abb4e906557215542e7d57105b52123fcf798d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 20:29:20 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
+import About from './components/About'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
     <Navbar/>
     <Home/>
     <SocialLinks/>
+    <About/>
      </div>
      
     </>

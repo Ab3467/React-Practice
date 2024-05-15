681f0e8bc7dc97c2c4e5f394e9327378f74551a5
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:00:37 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import SocialLinks from './components/SocialLinks'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
     <div>
     <Navbar/>
     <Home/>
+    <SocialLinks/>
      </div>
      
     </>
--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaGithub,FaLinkedin } from 'react-icons/fa'
+import {HiOutlineMail} from "react-icons/hi"
+import {BsFillPersonLinesFill} from "react-icons/bs"
 
 export default function SocialLinks() {
   return (
-    <div>
-      
+    <div className='flex flex-col top-[35] left-0 fixed'>
+      <ul>
+        <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
+            <a href="" className=''>
+            <>
+             Linkedin <FaLinkedin/>
+            </>
+            </a>
+        </li>
+      </ul>
     </div>
   )
 }

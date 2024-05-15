74b361fb09aa4c56d3ce8bb800a566530b791339
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 20:38:50 +0500

    Changes

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -10,6 +10,8 @@ export default function About() {
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero amet sit aspernatur laboriosam consequatur. Expedita, temporibus culpa molestiae eligendi fuga dignissimos, hic earum dolorem non consectetur modi accusantium numquam maiores odio minima reprehenderit!
                 </p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quam adipisci omnis ratione quia similique asperiores blanditiis, doloribus eos? Quod quas iusto ab aspernatur ducimus hic, expedita tempora minima architecto facilis sint eaque placeat aperiam cum harum nisi ullam dolores magnam laboriosam. Placeat tempora veritatis minus suscipit omnis cumque quas.</p>
             </div>
         </div>
     )

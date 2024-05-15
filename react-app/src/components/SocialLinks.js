20ce31da1cf2290d7d369f8f62149b24e11c059f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:44:25 +0500

    Changes

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -4,19 +4,58 @@ import {HiOutlineMail} from "react-icons/hi"
 import {BsFillPersonLinesFill} from "react-icons/bs"
 
 export default function SocialLinks() {
+    const links = [
+        {
+          id:1,
+          child:(
+            <>
+            Linkedin <FaLinkedin size={30}/>
+            </>
+          ),
+          href: "https://www.linkedin.com/in/ahmad-abu-bakr-481270254/"
+        },
+        {
+            id:2,
+            child:(
+              <>
+              Github <FaGithub size={30}/>
+              </>
+            ),
+            href: "https://github.com/Ab3467",
+            style:''
+          },
+          {
+            id:3,
+            child:(
+               <>
+               Mail <HiOutlineMail size={30}/>
+               </>
+            ),
+            href: "https://www.linkedin.com/in/ahmad-abu-bakr-481270254/"
+          },
+          {
+            id:4,
+            child:(
+             <>
+              Resume <BsFillPersonLinesFill size={30}/>
+             </>
+            ),
+            href: "https://www.linkedin.com/in/ahmad-abu-bakr-481270254/",
+            style: 'rounded-br-md',
+            download: true
+          },
+        
+    ]
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] rounded-md bg-gray-500 hover:rounded-none duration-500'>
-            <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
-            <> Linkedin <FaLinkedin size={30}/></>
-            </a>
-        </li>
-        {/* <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500'>
-            <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
-            <> Github <FaGithub size={40} className='pt-2 pl-3'/></>
-            </a>
-        </li> */}
+       {links.map((id,child,download,href,style)=>(
+<li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] rounded-md bg-gray-500 hover:rounded-none duration-500'+ " " +style}>
+        <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
+        <> {link} <FaLinkedin size={30}/></>
+        </a>
+    </li>
+       ))}
       </ul>
     </div>
   )

fcc961f8e8fcddb0eab3cf9997c502803a0f7983
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 19:13:16 +0500

    Changes

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -5,13 +5,16 @@ import {BsFillPersonLinesFill} from "react-icons/bs"
 
 export default function SocialLinks() {
   return (
-    <div className='flex flex-col top-[35] left-0 fixed'>
+    <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
         <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
-            <a href="" className=''>
-            <>
-             Linkedin <FaLinkedin/>
-            </>
+            <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
+            <> Linkedin <FaLinkedin size={30}/></>
+            </a>
+        </li>
+        <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
+            <a href="https://www.linkedin.com/in/ahmad-abu-bakr-481270254/" className=' flex justify-between w-full text-white items-center'>
+            <> Github <FaGithub size={30} className='pt-3'/></>
             </a>
         </li>
       </ul>

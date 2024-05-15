94b1e72e65cf89147f543e91a5c2761ff4b2a76f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-15 18:07:17 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {FaBars,FaTimes} from 'react-icons/fa'
 
 export default function Navbar() {
-    const [nav,setNav] = useState(true)
+    const [nav,setNav] = useState()
     const links =[
         {
           id: 1,
@@ -34,14 +34,16 @@ export default function Navbar() {
         <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
          ))}
         </ul>
-        <div onClick={()=> setNav(!nav)} className=" cursor-pointer pr-4 z-10 text-gray-500">
-           {nav ? <FaBars size={30}/> : <FaTimes size={30}/>}
+        <div onClick={()=> setNav(!nav)} className=" cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+           {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
         </div>
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-grey-800 text-grey-500'>
+
+        {nav && <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-grey-800 text-grey-500'>
         {links.map(({id,link})=>(
         <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>
          ))}
-         </ul>
+         </ul>}
+        
     </div>
   )
 }

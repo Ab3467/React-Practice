ae228b8aad2f5e0445ab548a5e1a1c6c5d41b110
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 19:50:26 +0500

    Changes

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -13,9 +13,11 @@ export default function Contact() {
             <form action="" className='flex flex-col w-full md:w-1/2'>
                 <input type="text" name="name" placeholder="Enter your Name" 
                 className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'/>
+
                 <input type="email" name="name" placeholder="Enter your Email" 
-                className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'/>
-                <textarea name="message" rows="10"
+                className='my-4 p-2 bg-transparent border-2 roun text-white focus:outline-none'/>
+
+                <textarea name="message" rows="10" placeholder='Enter you message here'
                 className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'></textarea>
                 <button className=' text-white bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 mx-auto my-8 flex items-center
                 rounded-md hover:scale-110 duration-300'>Let's Talk</button>

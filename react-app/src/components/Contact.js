08f6c0355f44409f050946d1b84c7f1c9feb1e19
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 19:42:17 +0500

    Changes

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -17,7 +17,7 @@ export default function Contact() {
                 className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'/>
                 <textarea name="message" rows="10"
                 className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'></textarea>
-                <button className=' text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 mx-auto my-8 flex items-center
+                <button className=' text-white bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 mx-auto my-8 flex items-center
                 rounded-md hover:scale-110 duration-300'>Let's Talk</button>
             </form>
         </div>

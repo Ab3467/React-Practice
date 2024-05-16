5bbdb5ae7e58890f11304d480068f423ad17eb0d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 19:47:56 +0500

    Changes

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>
-      <div className="">
-        <div className="">
-            <p>Contact</p>
-            <p>Here is the way to get in Touch with me</p>
+    <div className=" w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white" name="contact">
+      <div className=" flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+            <p className='text-4xl font-bold border-b-4 border-gray-500 inline'>Contact</p>
+            <p className='py-6'>Here is the way to get in Touch with me</p>
         </div>
 
-        <div className="">
-            <form action="">
+        <div className='flex justify-center items-center'>
+            <form action="" className='flex flex-col w-full md:w-1/2'>
                 <input type="text" name="name" placeholder="Enter your Name" 
                 className=' p-2 bg-transparent border-2 roun text-white focus:outline-none'/>
                 <input type="email" name="name" placeholder="Enter your Email" 

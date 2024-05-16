a5459a41c9674d8aacd960bcd2bdecf5e8b02735
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 18:38:11 +0500

    Changes

--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import css from "../assets/css.png"
+import react from "../assets/react.png"
+import javascript from "../assets/javascript.png"
+import html from "../assets/html.png"
+import node from "../assets/node.png"
+import tailwind from "../assets/tailwind.png"
+import graphql from "../assets/graphql.png"
+import github from "../assets/github.png"
 
 export default function Experience() {
   return (

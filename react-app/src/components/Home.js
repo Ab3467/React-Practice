6ec3127f5032e15b2c31a92519f8c9bd510842a1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 20:12:44 +0500

    Changes

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from "../assets/Ahmad.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from 'react-scroll';
 
 export default function Home() {
     return (
@@ -15,12 +16,13 @@ export default function Home() {
                         Internship at Vyro.
                     </p>
                     <div>
-                        <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer' >
-                            Portfolio
-                            <span className=' group-hover:rotate-90 scroll-smooth duration-300'>
+                        <Link to='portfolio' smooth duration={500} 
+                        className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer' >
+                        Portfolio
+                        <span className=' group-hover:rotate-90 scroll-smooth duration-300'>
                                 <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-                            </span>
-                        </button>
+                        </span>
+                        </Link>
                     </div>
                 </div>
                 <div className="md:w-1/2 flex justify-center">

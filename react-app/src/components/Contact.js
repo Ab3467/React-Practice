6fe1cb205e15e2d417cbcf6d12dec58694365c17
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 19:35:09 +0500

    Changes

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      Contact
+      <div className="">
+        <div className="">
+            <p>Contact</p>
+            <p>Here is the way to get in Touch with me</p>
+        </div>
+
+        <div className="">
+            <form action="">
+                <input type="text" name="name" placeholder="Enter your Name" />
+            </form>
+        </div>
+      </div>
     </div>
   )
 }

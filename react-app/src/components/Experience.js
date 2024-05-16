50fef525d88b757deac1a6cb4bb14771c7e592c2
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 18:47:42 +0500

    Changes

--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -18,8 +18,8 @@ export default function Experience() {
         </div>
         <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center px-8 py-12 sm:px-0">
             <div className=" shadow-md hover:scale-105 duration-500 py-2 rounded-lg">
-                <img src={html} alt="html" />
-                <p>HTML</p>
+                <img src={html} alt="html" className='w-20 mx-auto'/>
+                <p className='mt-4'>HTML</p>
             </div>
         </div>
       </div>

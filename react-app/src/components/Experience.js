6246b6d8354bf8ec71991580ffec7f5e5e849dad
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 18:46:57 +0500

    Changes

--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -10,8 +10,19 @@ import github from "../assets/github.png"
 
 export default function Experience() {
   return (
-    <div>
-      
+    <div className=' bg-gradient-to-b from-gray-800 to-black w-full h-screen' name="experience">
+      <div className=" max-w-screen-lg w-full h-full mx-auto p-4 flex flex-col justify-center text-white">
+        <div>
+            <p className="text-4xl font-bold border-b border-gray-500 p-2 inline">Experience</p>
+            <p className='py-6'>These are the Technologies I've worked with</p>
+        </div>
+        <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center px-8 py-12 sm:px-0">
+            <div className=" shadow-md hover:scale-105 duration-500 py-2 rounded-lg">
+                <img src={html} alt="html" />
+                <p>HTML</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }

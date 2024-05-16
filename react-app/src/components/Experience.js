1835c6250ef06d33a8b890bbda7d7c0a28ddb9a1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 20:24:11 +0500

    Changes

--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -9,83 +9,80 @@ import graphql from "../assets/graphql.png"
 import github from "../assets/github.png"
 import nextjs from "../assets/nextjs.png"
 
-
 export default function Experience() {
-    const links=[
+    const links = [
         {
-          id:1,
-          Tech:"HTML",
+          id: 1,
+          Tech: "HTML",
           src: html,
-          style: " shadow-orange-500"
+          style: "shadow-orange-500"
         },
         {
-            id:2,
-            Tech:"CSS",
+            id: 2,
+            Tech: "CSS",
             src: css,
-            style: " shadow-yellow-500"
+            style: "shadow-blue-500"
         },
         {
-            id:3,
-            Tech:"REACT",
+            id: 3,
+            Tech: "REACT",
             src: react,
-            style: " shadow-blue-600"
+            style: "shadow-blue-600"
         },
         {
-            id:4,
-            Tech:"JAVASCRIPT",
+            id: 4,
+            Tech: "JAVASCRIPT",
             src: javascript,
-            style: " shadow-gray-500"
+            style: "shadow-yellow-500"
         },
         {
-            id:5,
-            Tech:"TAILWIND",
+            id: 5,
+            Tech: "TAILWIND",
             src: tailwind,
-            style: " shadow-red-500"
+            style: "shadow-green-500"
         },
         {
-            id:6,
-            Tech:"NODE",
+            id: 6,
+            Tech: "NODE",
             src: node,
-            style: " shadow-white"
+            style: "shadow-green-500"
         },
         {
-            id:7,
-            Tech:"GITHUB",
+            id: 7,
+            Tech: "GITHUB",
             src: github,
-            style: " shadow-pink-500"
+            style: "shadow-gray-500",
         },
         {
-            id:8,
-            Tech:"GRAPHQL",
+            id: 8,
+            Tech: "GRAPHQL",
             src: graphql,
-            style: " shadow-blue-600"
+            style: "shadow-pink-500"
         },
         {
-            id:9,
-            Tech:"NEXTJS",
+            id: 9,
+            Tech: "NEXTJS",
             src: nextjs,
-            style: " shadow-orange-500"
+            style: "shadow-gray-500"
         },
-
     ]
-  return (
-    <div className=' bg-gradient-to-b from-gray-800 to-black w-full h-screen' name="experience">
-      <div className=" max-w-screen-lg w-full h-full mx-auto p-4 flex flex-col justify-center text-white">
-        <div>
-            <p className="text-4xl font-bold border-b border-gray-500 p-2 inline sm:pt-3">Experience</p>
-            <p className='py-6'>These are the Technologies I've worked with</p>
-        </div>
-        <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center px-8 py-12 sm:px-0">
-            
-            {links.map(({id,src,Tech,style})=>(
-               <div className={" shadow-md hover:scale-105 duration-500 py-2 rounded-lg"+ " "+ style} key={id}>
-               <img src={src} alt="html" className='w-20 mx-auto'/>
-               <p className='mt-4'>{Tech}</p>
-            </div>
-            ))}
 
+    return (
+        <div className='bg-gradient-to-b from-gray-800 to-black w-full h-screen' name="experience">
+            <div className="max-w-screen-lg w-full h-full mx-auto p-4 flex flex-col justify-center text-white">
+                <div>
+                    <p className="text-4xl font-bold border-b border-gray-500 p-2 inline sm:pt-3">Experience</p>
+                    <p className='py-6'>These are the Technologies I've worked with</p>
+                </div>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center px-8 py-12 sm:px-0">
+                    {links.map(({ id, src, Tech, style }) => (
+                        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`} key={id}>
+                            <img src={src} alt={Tech} className='w-20 mx-auto'/>
+                            <p className='mt-4'>{Tech}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }

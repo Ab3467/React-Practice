7aeb10c543351cee21d491e202ccb1f79c7e8f32
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-16 18:55:48 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
+import Experience from './components/Experience'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <About />
         <SocialLinks />
         <Portfolio/>
+        <Experience/>
       </div>
 
     </>

2f8b84dcccec5501f6ec9442bc65681fa03bfa96
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 16:11:19 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -33,11 +33,11 @@ export default function Navbar() {
     ]
     return (
         <div className=' flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
-            <div><h1 className='text-5xl font-signature ml-2'>Portfolio</h1></div>
+            <div><h1 className='text-5xl font-signature ml-2'>Ahmad abu bakr</h1></div>
 
             <ul className=' hidden md:flex'>
                 {links.map(({ id, link }) => (
-                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'><Link to={link} smooth duration={500}>{link}</Link> </li>
+                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200  hover:text-white'><Link to={link} smooth duration={500}>{link}</Link> </li>
                 ))}
             </ul>
             <div onClick={() => setNav(!nav)} className=" cursor-pointer pr-4 z-10 text-gray-500 md:hidden">

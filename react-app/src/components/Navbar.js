0c2713d194a2e4eaff6e541e460fe23ecaa2d20c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 16:05:29 +0500

    Changes

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
-import {Link} from "react-scroll"
+import { Link } from "react-scroll"
 
 export default function Navbar() {
     const [nav, setNav] = useState()
@@ -48,8 +48,8 @@ export default function Navbar() {
                 {links.map(({ id, link }) => (
                     <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
                         <Link
-                        onClick={()=> setNav(!nav)}
-                        to={link} smooth duration={500}>{link}</Link></li>
+                            onClick={() => setNav(!nav)}
+                            to={link} smooth duration={500}>{link}</Link></li>
                 ))}
             </ul>}
 
